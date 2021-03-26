fd491b9b46afde36b73abd0170afd6e2b48973f2
Joachmak <joachmak@users.noreply.github.com>
2021-03-26 13:29:04 +0100

    Add spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import {CircularProgress} from "@material-ui/core";
 
 function App() {
   return (
     <div className="App">
-        <p>Test</p>
-        <p>Hmm is this working?</p>
+        <p>Work in progress...</p>
+        <CircularProgress />
     </div>
   );
 }
